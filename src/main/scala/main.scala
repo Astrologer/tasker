c789e2ac075b409958c4c81d8617495d21bd8869
Denis <Astrologer@users.noreply.github.com>
2020-12-10 22:03:30 +0300

    subjob example

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -2,7 +2,7 @@ import cats.effect.{ConcurrentEffect, ExitCode, IO, IOApp}
 import tasker.{Job, JobExecutionContext, JobRunnerBase, SumIntCable, Task}
 
 class Tasks[F[_]: ConcurrentEffect] {
-  val t1: Task[Int, Int, F] = Task.sync((x: Int) => x - 3, "sum")
+  val t1: Task[Int, Int, F] = Task.sync((x: Int) => x - 3, "sub")
   val t2: Task[Int, Int, F] = Task.sync((x: Int) => x + 2, "add")
   val t3: Task[Int, Int, F] = Task.sync((x: Int) => x * x, "mul")
 }
@@ -13,16 +13,26 @@ object main extends IOApp {
   import tasks._
 
   def run(args: List[String]): IO[ExitCode] = {
-    val job = Job(t3, t1, JobRunnerBase, resultCable = SumIntCable)
+    val job = Job(t3, t1, JobRunnerBase, resultCable = SumIntCable, name = "job")
       .dep(t3, t2)
       .dep(t2, t1)
       .dep(t3, t1)
 
+    val job2 = Job(t1, t2, JobRunnerBase)
+      .dep(t1, job)
+      .dep(job, t2)
+
     for {
-      rJob <- job.launch(7)
       rTask <- t3.run(7)
-      _ <- IO { println(s"ok = $rJob / $rTask") }
-      _ <- IO { ex.terminate() }
+      _ = println(s"Result: $rTask\n")
+
+      rJob <- job.launch(7)
+      _ = println(s"Result: $rJob\n")
+
+      r <- job2.launch(10)
+      _ = println(s"Result: $r\n")
+
+      _ = ex.terminate()
     } yield ExitCode.Success
   }
 }
